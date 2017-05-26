16380c7427169394cca7dcd4de2ec565394e0e99
Vladimir Minkin <vladimir.minkin@gmail.com>
2017-05-26 22:53:22 +0200

    Make it run on Linux\Ubuntu (Eclipse)

--- src/Application.java
+++ src/Application.java
@@ -3,39 +3,28 @@ import java.awt.EventQueue;
 import app.ApplicationFacade;
 import consts.notifications.ApplicationNotifications;
 
-public class Application implements Runnable {
-
+public class Application implements Runnable 
+{
 	private static final ApplicationFacade shell = new ApplicationFacade();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		EventQueue.invokeLater(new Application());
 	}
 	
 	public void run() 
 	{
 		shell.startup(this);
-		
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				shell.sendNotification(ApplicationNotifications.APPLICATION_CLOSED);
-			}
-		});
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public Application() {
-		initialize();
-	}
+	public Application() {	initialize(); }
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
-		
-	}
+	private void initialize() { }
 }
+
